9375a4b8867643ed691af5f927399b2774aa0ee1
selimgunaydin <selimg611@gmail.com>
2022-08-10 15:33:47 +0300

    İkincil sonuç ekranı eklendi.

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 const buttons = document.querySelectorAll("button");
 const display = document.querySelector(".display");
+const displayResult = document.querySelector(".display-result");
 
 $("#dontClick").on({
   click: function () {
@@ -13,12 +14,15 @@ buttons.forEach(function (button) {
 
 function calculate(event) {
   const clickedButtonValue = event.target.value;
-  
   if (clickedButtonValue === "=") {
     if (display.value !== "") {
-      display.value = eval(display.value);
+      displayResult.value = display.value + "=" + eval(display.value);
+      display.value = "";
     }
   } else if (clickedButtonValue === "AC") {
+    display.value = "";
+    displayResult.value = "";
+  } else if (clickedButtonValue === "C") {
     display.value = "";
   } else {
     display.value += clickedButtonValue;
